edd3befeb93147ad7dd5a821fd4ae24069874508
Hristo Bakalov <hristobak@gmail.com>
2015-11-07 15:10:00 +0100

    Initial commit of the whole project Starting page -done

---
+++ src/uchem/StartWindow.java
@@ -0,0 +1,5 @@
+package uchem;
+
+public class StartWindow {
+
+}
---
+++ src/uchem/uchemUI.java
@@ -0,0 +1,194 @@
+package uchem;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.InputStream;
+import java.awt.event.ActionEvent;
+import java.awt.Canvas;
+import javax.swing.JLabel;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class uchemUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					uchemUI window = new uchemUI();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public uchemUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		// TODO: remove code repetition;
+		// TODO: improve code quality;
+		// TODO: set variables for dimensions and positions;
+		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 600, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+
+		JPanel topPanel = new JPanel();
+		// TODO: make only the bottom border appear;
+
+		topPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		topPanel.setBounds(0, 0, 600, 100);
+		topPanel.setBackground(Color.decode("#126ad9"));
+		topPanel.setLayout(null);
+		frame.getContentPane().add(topPanel);
+
+		JPanel mainWindow = new JPanel();
+		mainWindow.setBounds(0, 100, 600, 400);
+		frame.getContentPane().add(mainWindow);
+		mainWindow.setBackground(Color.decode("#568ed4"));
+		mainWindow.setLayout(null);
+		
+		
+		mainWindow.setVisible(false);
+		
+		JPanel levelsPanel = levels( mainWindow);
+		//initially should be false
+		levelsPanel.setVisible(true);
+		frame.getContentPane().add(levelsPanel);
+		
+		JLabel logoLabel = new JLabel("");
+		Image logo = new ImageIcon(this.getClass().getResource("/logo1.png")).getImage();
+		logoLabel.setIcon(new ImageIcon(logo));
+		logoLabel.setBounds(0, 11, 65, 80);
+		topPanel.add(logoLabel);
+
+		int positionY = (topPanel.getWidth() - 229)/2;
+		JLabel uChemLabel = new JLabel("");
+		Image uChemLogo = new ImageIcon(this.getClass().getResource("/uChem1.png")).getImage();
+		uChemLabel.setIcon(new ImageIcon(uChemLogo));
+		uChemLabel.setBounds(positionY, 11, 229, 80);
+		topPanel.add(uChemLabel);
+		
+		
+		
+		mainWindow(mainWindow, levelsPanel);
+
+		// InputStream input = classLoader.getResourceAsStream("logo.png");
+	}
+
+	public void mainWindow(JPanel mainWindow, JPanel levelsPanel) {
+		JButton start = createButton(mainWindow, "START", 134);
+		mainWindow.add(start);
+		
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mainWindow.setVisible(false);
+				levelsPanel.setVisible(true);
+			}
+		});
+
+		JButton dictionary = createButton(mainWindow,"DICTIONARY", 194);
+		mainWindow.add(dictionary);
+
+		dictionary.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mainWindow.setVisible(false);
+			}
+		});
+
+		JButton aboutUs = createButton(mainWindow,"ABOUT US", 254);
+		mainWindow.add(aboutUs);
+		
+		aboutUs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mainWindow.setVisible(false);
+			}
+		});
+	}
+
+	public JButton createButton(JPanel window, String name, int positionY) {
+		int buttonWidth = 190;
+		int buttonHeight = 50;
+		JButton currentButton = new JButton(name);
+		currentButton.setForeground(Color.WHITE);
+		currentButton.setFont(new Font("Verdana", Font.PLAIN, 24));
+		
+		int windowWidth = window.getWidth();
+		int positionX = (windowWidth - buttonWidth) /2; 
+		currentButton.setBounds(positionX, positionY, buttonWidth, buttonHeight);
+		currentButton.setBackground(Color.decode("#1458ae"));
+
+		return currentButton;
+	}
+	
+	public JPanel levels (JPanel mainWindow){
+		JPanel levelsPanel = new JPanel();
+		levelsPanel.setBounds(0, 100, 600, 400);
+		levelsPanel.setBackground(Color.decode("#568ed4"));
+		levelsPanel.setLayout(null);
+		JButton cLevel = createButton(levelsPanel, "C Level", 114);
+		levelsPanel.add(cLevel);
+		
+		JButton bLevel = createButton(levelsPanel, "B Level", 174);
+		levelsPanel.add(bLevel);
+		
+		JButton aLevel = createButton(levelsPanel, "A Level", 234);
+		levelsPanel.add(aLevel);
+		
+		JButton combined = createButton(levelsPanel, "Combined", 294);
+		levelsPanel.add(combined);
+		
+		int positionY = (levelsPanel.getWidth() - 300)/2;
+		JLabel lblNewLabel = new JLabel(" CHOOSE YOUR LEVEL");
+		lblNewLabel.setBounds(positionY, 40, 300, 50);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 24));
+		levelsPanel.add(lblNewLabel);
+		
+		JButton backButton = new JButton("Back");
+		int buttonWidth = 100;
+		int buttonHeight = 30;
+		
+		backButton.setForeground(Color.WHITE);
+		backButton.setFont(new Font("Verdana", Font.PLAIN, 20));
+		backButton.setBounds(0, 0, buttonWidth, buttonHeight);
+		backButton.setBackground(Color.decode("#1458ae"));
+		backButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				levelsPanel.setVisible(false);
+				mainWindow.setVisible(true);
+			}
+		});
+		levelsPanel.add(backButton);
+		return levelsPanel;
+		
+	}
+}
